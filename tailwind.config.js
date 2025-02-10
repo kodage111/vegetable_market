c82c643a458d41504eb4070ebe802b204bb13397
kodage111 <tenetampo@gmail.com>
2025-02-10 23:56:38 +0100

    animation on about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,14 +39,25 @@ module.exports = {
         '50%': { transform: 'translate(-10%, 30%) scale(0.8)' },
         '75%': { transform: 'translate(10%, 80%) scale(1.1)' },
         '100%': { transform: 'translate(0, 0) scale(0.4)' },
-      }
+      },
+      "animate-in": {
+        '0%': { opacity: 0.2, transform: 'scale(0.8) translate(0, 10%)' },
+        '100%': { opacity: 1, transform: 'scale(1) translate(0, 0)' },
+      },
+      appear: {
+        '0%': { opacity: 0, transform: "rotate(220deg) translate(-20%, 0)" },
+        '100%': { opacity: 0.3, transform: "rotate(220deg) translate(0, 0)" },
+      },
     },
     animation: {
       rotation: "rotate 15s linear infinite",
       scaleUp: "scale 3s linear infinite",
       "move-around": "move-around 20s infinite ease-in-out",
       "move-around": "move-around 25s infinite alternate ease-in-out",
-      moveOnY: "moveUp 3s linear infinite"
+      moveOnY: "moveUp 3s linear infinite",
+      'reveal': 'animate-in 0.5s ease-out both',
+      'appear': 'appear 0.5s ease-out both',
+      "fade-down": "fadeDown 0.5s ease-out both",
     },
     container: {
       center: true,
